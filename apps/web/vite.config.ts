a54b13f0b37a162c521e0b96041ad36fa6f651b7
krapjost <krapjost@gmail.com>
2023-04-02 14:19:31 +0900

    choir: delete comment

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 
-// clearScreen: false,
-// envPrefix: ["VITE_", "TAURI_"],
-// target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
-// minify: process.env.TAURI_DEBUG ? false : "esbuild",
-// sourcemap: !!process.env.TAURI_DEBUG,
-
 export default defineConfig({
   plugins: [solidPlugin()],
   server: {
