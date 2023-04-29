775281f37a50727cc9d9afc3cdaefbfccabc19b2
jcha0713 <joocha0713@gmail.com>
2023-04-29 16:24:45 +0900

    chore: add delay to reveal animation

--- pkgs/ui/src/Nav.tsx
+++ pkgs/ui/src/Nav.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import { Accessor, JSX, Show } from 'solid-js'
 import { Button } from './Button'
 import { Icon } from './Icon'
@@ -12,9 +13,16 @@ type Component = (props: propType & { ref?: HTMLDivElement }) => JSX.Element
 const Nav: Component = (props) => {
   return (
     <div class='flex p-2 justify-between'>
-      <div class='flex flex-row'>
+      <div
+        class={clsx('flex flex-row opacity-0 transition-opacity delay-300', {
+          'opacity-100': !props.isSidebarOpen(),
+        })}
+      >
         <Show when={!props.isSidebarOpen()}>
-          <Button onclick={props.toggleSidebar} size={'icon'}>
+          <Button
+            onclick={props.toggleSidebar}
+            size={'icon'}
+          >
             <Icon name='IconLayoutSidebarLeftExpand' />
           </Button>
         </Show>
