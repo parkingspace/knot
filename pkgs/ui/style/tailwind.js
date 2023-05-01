eb3a745123025b7ae02b48d70cd81df68d4690e6
jcha0713 <joocha0713@gmail.com>
2023-05-01 22:38:45 +0900

    chore: use class strategy to toggle dark mode

--- pkgs/ui/style/tailwind.js
+++ pkgs/ui/style/tailwind.js
@@ -2,6 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = function(context) {
   const config = {
+    darkMode: 'class',
     content: [
       '../../pkgs/{editor/*.{ts,tsx},ui/src/**/*.{ts,tsx}}',
       '../../apps/web/src/**/*.{ts,tsx}',
