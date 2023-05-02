d3e17407a8afa00fadbac5efb8aa1a416522f646
jcha0713 <joocha0713@gmail.com>
2023-05-02 12:25:13 +0900

    chore: remove header component from ui dir

--- pkgs/ui/src/index.ts
+++ pkgs/ui/src/index.ts
@@ -1,7 +1,6 @@
 export * from './BaseLayout'
 export * from './Button'
 export * from './FlexBox'
-export * from './Header'
 export * from './Icon'
 export * from './Sidebar'
 export * from './TextArea'
