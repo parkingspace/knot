7d0bcce8753df954e4deac19c8ff6bd26bb81109
jcha0713 <joocha0713@gmail.com>
2023-05-05 14:41:18 +0900

    feat: add basic theme toggle button

--- pkgs/editor/interface/ColorSchemeToggle.tsx
+++ pkgs/editor/interface/ColorSchemeToggle.tsx
@@ -1,13 +1,26 @@
+import { createEffect, createSignal, Show } from 'solid-js'
 import { Button, Icon } from 'ui'
+
+type theme = 'light' | 'dark'
 
 // TODO: use localstorage to persist the color scheme
 export const ColorSchemeToggle = () => {
+  const [theme, setTheme] = createSignal<theme>('light')
+
+  createEffect(() => {
+    const root = document.documentElement
+    root.classList.toggle('dark', theme() === 'dark')
+  })
+
   return (
     <Button
-      onclick={() => document.documentElement.classList.toggle('dark')}
+      onclick={() =>
+        setTheme((theme) => (theme === 'light' ? 'dark' : 'light'))}
       size={'icon'}
     >
-      <Icon name='IconSunFilled' />
+      <Show when={theme() === 'dark'} fallback={<Icon name='IconMoonFilled' />}>
+        <Icon name='IconSunFilled' />
+      </Show>
     </Button>
   )
 }
