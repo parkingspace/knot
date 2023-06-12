8c5832b7a719623436b0c6a0d70654bb0c552666
krapjost <krapjost@gmail.com>
2023-06-12 15:08:10 +0900

    krapjost/feat/featur-toggle: add feature toggle modal

--- pkgs/editor/src/features/index.tsx
+++ pkgs/editor/src/features/index.tsx
@@ -1,8 +1,16 @@
-import { children, Component, For, JSXElement, Show } from 'solid-js'
+import {
+  children,
+  Component,
+  createSignal,
+  For,
+  JSXElement,
+  Show,
+} from 'solid-js'
 import { useKnotEditor } from '..'
 import type { FeatureName, Features, FeatureTypes } from '../types/configTypes'
 import { useFeatureConfig } from './configStore'
 
+import clsx from 'clsx'
 import { initCaret } from './caret'
 import { initHeader } from './header'
 import { initSearch } from './search'
@@ -10,54 +18,90 @@ import { initSidebar } from './sidebar'
 import { initTypewriter } from './typewriter'
 import { initWhichkey } from './whichkey'
 
-interface FeaturesProps {
-  children: JSXElement
-}
-
-function getFeatureComponent<FeatureName extends keyof FeatureTypes>(
-  name: FeatureName,
-): FeatureTypes[FeatureName] {
-  const components = {
-    caret: initCaret,
-    header: initHeader,
-    search: initSearch,
-    sidebar: initSidebar,
-    typewriter: initTypewriter,
-    whichkey: initWhichkey,
-  }
-
-  return components[name]
-}
-
-export function Features(props: FeaturesProps) {
+function FeatureToggleModal(props: {
+  show: boolean
+  toggle: () => void
+}) {
   const { featureState, toggleFeature } = useFeatureConfig()
-  const features = children(() => props.children)
-  const featuresArray = features.toArray() as unknown as FeatureProps[]
+
+  return (
+    <div
+      class={clsx('fixed inset-0 z-50 flex items-center justify-center', {
+        'hidden': !props.show,
+      })}
+    >
+      <div onclick={props.toggle} class='absolute inset-0 bg-black opacity-50'></div>
+      <div class='relative bg-white rounded-lg w-1/2'>
+        <div class='p-4'>
+          <div class='flex flex-col justify-between'>
+            <div class='font-semibold'>Features</div>
+            <div class='flex flex-row gap-x-2 justify-between'>
+              caret
+              <button onclick={() => toggleFeature('caret')}>
+                {featureState.caret.enabled ? 'Disable' : 'Enable'}
+              </button>
+            </div>
+            <div class='flex flex-row gap-x-2 justify-between'>
+              header
+              <button onclick={() => toggleFeature('header')}>
+                {featureState.header.enabled ? 'Disable' : 'Enable'}
+              </button>
+            </div>
+            <div class='flex flex-row gap-x-2 justify-between'>
+              search
+              <button onclick={() => toggleFeature('search')}>
+                {featureState.search.enabled ? 'Disable' : 'Enable'}
+              </button>
+            </div>
+            <div class='flex flex-row gap-x-2 justify-between'>
+              sidebar
+              <button onclick={() => toggleFeature('sidebar')}>
+                {featureState.sidebar.enabled ? 'Disable' : 'Enable'}
+              </button>
+            </div>
+            <div class='flex flex-row gap-x-2 justify-between'>
+              typewriter
+              <button onclick={() => toggleFeature('typewriter')}>
+                {featureState.typewriter.enabled ? 'Disable' : 'Enable'}
+              </button>
+            </div>
+            <div class='flex flex-row gap-x-2 justify-between'>
+              whichkey
+              <button onclick={() => toggleFeature('whichkey')}>
+                {featureState.whichkey.enabled ? 'Disable' : 'Enable'}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export function Features() {
+  const { featureState, toggleFeature } = useFeatureConfig()
+  const { caret, header, search, sidebar, typewriter, whichkey } = featureState
+  const [showModal, setShowModal] = createSignal(false)
+  const toggle = () => setShowModal(!showModal())
 
   const { editor } = useKnotEditor()
   editor.view.dom.spellcheck = false
 
   return (
     <>
-      <For each={featuresArray}>
-        {(feature) => {
-          return (
-            <>
-              <Show when={featureState[feature.name].enabled}>
-                {getFeatureComponent(feature.name)()}
-              </Show>
-            </>
-          )
-        }}
-      </For>
+      <FeatureToggleModal show={showModal()} toggle={toggle} />
+      <div class='fixed bottom-0 left-0 z-50 text-editorFg p-4 font-semibold'>
+        <button onclick={() => toggle()}>
+          Feature
+        </button>
+      </div>
+
+      {caret.enabled && initCaret()}
+      {header.enabled && initHeader()}
+      {search.enabled && initSearch()}
+      {sidebar.enabled && initSidebar()}
+      {typewriter.enabled && initTypewriter()}
+      {whichkey.enabled && initWhichkey()}
     </>
   )
-}
-
-interface FeatureProps {
-  name: FeatureName
-}
-
-export const Feature: Component<FeatureProps> = (props) => {
-  return props as unknown as JSXElement
 }
